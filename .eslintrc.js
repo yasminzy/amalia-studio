e3517e3a2d119dc7c5f973b079137e4ce58f01b1
Yasmin Zulfati Yusrina <yzyusrina@ymail.com>
2019-01-02 13:58:31 +0800

    Turn off naming rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "vue/component-name-in-template-casing": "off",
     "vue/v-bind-style": "off"
   },
   parserOptions: {
