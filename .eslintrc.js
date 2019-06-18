546ae3cbe92bec5557163b9d7cf0194ce12b1470
Yasmin Zulfati Yusrina <yzyusrina@ymail.com>
2019-06-18 10:08:21 +0700

    Update rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,8 @@ module.exports = {
   },
   extends: ["plugin:vue/recommended", "@vue/prettier"],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "vue/component-name-in-template-casing": "off",
-    "vue/v-bind-style": "off"
+    "no-console": "off",
+    "no-debugger": "off"
   },
   parserOptions: {
     parser: "babel-eslint"
